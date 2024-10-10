74e030da4f3fcf3aa6d81dd1fcd9859f43395350
GMMushroom <retromushroomgaming@gmail.com>
2024-10-10 12:15:30 +1100

    Week 3 - UI & Win Panel
    Added Text and panels to the game, as well as a bonus fillamount radial wheel and aris dance.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +11,17 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     private int pickupCount;
     private Timer timer;
+    private bool gameOver = false;
+    private int totalPickups;
+    private float pickupChunk;
+
+    [Header("UI")]
+    public TMP_Text pickupText;
+    public TMP_Text timerText;
+    public Image pickupImage;
+    public TMP_Text winTimeText;
+    public GameObject inGamePanel;
+    public GameObject winPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -17,16 +30,36 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         //Get the number of pickups in our scene
         pickupCount = GameObject.FindGameObjectsWithTag("Pickup").Length;
+        //Assign the total amount of pickups
+        totalPickups = pickupCount;
+
+        pickupImage.fillAmount = 0;
+
+        pickupChunk = 1.0f / pickupCount;
         //Run the CheckPickups() function
         CheckPickups();
         //Gets the timer Object
         timer = FindObjectOfType<Timer>();
+        //Start timer
         timer.StartTimer();
+        //Turn off Win Panel & Turn on In-Game Panel
+        winPanel.SetActive(false);
+        inGamePanel.SetActive(true);
+        //Return game state
+        gameOver = false;
+    }
+
+    private void Update()
+    {
+        timerText.text = "Time: " + timer.currentTime.ToString("F2");
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (gameOver == true)
+            return;
+
         //Store the Horizontal value in a float
         float moveHorizontal = Input.GetAxis("Horizontal");
 
@@ -46,6 +79,8 @@ public class PlayerController : MonoBehaviour
             Destroy(other.gameObject);
             //Decrement pickupCount
             pickupCount--;
+            //Fill pickupImage
+            pickupImage.fillAmount = pickupImage.fillAmount + pickupChunk;
             //Run CheckPickups() function again
             CheckPickups();
         }
@@ -53,7 +88,9 @@ public class PlayerController : MonoBehaviour
 
     private void CheckPickups()
     {
-        print("Pickups left: " + pickupCount);
+        //print("Pickups left: " + pickupCount);
+        pickupText.text = ("Pickups left: " + pickupCount);
+        pickupText.color = Color.yellow;
 
         if (pickupCount == 0)
         {
@@ -64,6 +101,20 @@ public class PlayerController : MonoBehaviour
 
     private void WinGame()
     {
-        print("CONGRATULATIONS!!! Your time was: " + timer.GetTime().ToString("F2"));
+        //pickupText.color = Color.green;
+        inGamePanel.SetActive(false);
+        winPanel.SetActive(true);
+        gameOver = true;
+        //print("CONGRATULATIONS!!! Your time was: " + timer.GetTime().ToString("F2"));
+        winTimeText.text = "Your time was: " + timer.GetTime().ToString("F2");
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    //Temporary Restart Function
+    public void ResetGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }
